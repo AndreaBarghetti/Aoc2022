06149f761c522307ddccac7f0b292666ceeb012b
Andrea Barghetti <32954484+AndreaBarghetti@users.noreply.github.com>
2022-12-01 17:54:10 +0100

    day1 vis

--- Day1/day1.R
+++ Day1/day1.R
@@ -20,3 +20,21 @@ max(elfs)
 # part2 ####
 sum(sort(elfs, decreasing = T)[1:3])
 
+
+# Visualization ####
+image <- ggplot(data=NULL,aes(x=seq_along(elfs), y=sort(elfs))) +
+  geom_point(size=.1,) +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "darkgreen",
+                                        colour = "black",
+                                        size = .5, linetype = "solid")) +
+  labs(x=NULL, y="Snacks")
+
+ggsave(plot = image,
+       filename = "image.png",
+       device = "png",
+       path = "Day1/",
+       width = 4,
+       height = 4, units = "cm")
+
+
