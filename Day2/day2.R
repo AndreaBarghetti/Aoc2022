b0597ebcc70aff79b510893d7aad2cf721f9e28d
Andrea Barghetti <32954484+AndreaBarghetti@users.noreply.github.com>
2022-12-02 13:39:31 +0100

    Update day2.R

--- Day2/day2.R
+++ Day2/day2.R
@@ -35,47 +35,6 @@ part2 <- function(input) {
 part2(input)
 
 
-# other solution
-part1 <- function(input) {
-  get_points <- function(game) {
-    hands <- str_split(game," ", simplify = T)
-    hands <- recode(hands, "A"=1,"B"=2,"C"=3,"X"=1,"Y"=2,"Z"=3)
-    points <- hands[2]
-    if(all(hands==c(1,2))|
-       all(hands==c(2,3))|
-       all(hands==c(3,1))) {points<-points+6}
-    if(hands[1]==hands[2]) {points<-points+3}
-    points
-  }
-
-  map_dbl(input, get_points) %>% sum()
-}
-
-part2 <-  function(input) {
-  get_points <- function(game) {
-    hands <- str_split(game," ", simplify = T)
-    hands <- recode(hands, "A"=1,"B"=2,"C"=3,"X"=0,"Y"=3,"Z"=6)
-    points <- hands[2]
-    if(hands[2]==3) {points<-points+hands[1]}
-    if(hands[2]==6) {
-      if(hands[1]==1) {points<-points+2}
-      if(hands[1]==2) {points<-points+3}
-      if(hands[1]==3) {points<-points+1}
-    }
-    if(hands[2]==0) {
-      if(hands[1]==1) {points<-points+3}
-      if(hands[1]==2) {points<-points+1}
-      if(hands[1]==3) {points<-points+2}
-    }
-    points
-  }
-
-  map_dbl(input, get_points) %>% sum()
-}
-
-part1(input)
-part2(input)
-
 # Visualization ####
 getpoints1 <- function(input) {
   games <- input %>% str_replace_all(c("A"="R","B"="P","C"="S",
